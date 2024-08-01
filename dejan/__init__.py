e5ab628667cf35b3bceb4c87a2b3baa840fdcf34
Dan Petrovic <dan.petrovic@dejan.com.au>
2024-08-01 17:31:54 +1000

    Initial commit with setup.py, README.md, and utility scripts

---
+++ dejan/__init__.py
@@ -0,0 +1,3 @@
+# dejan/__init__.py
+
+from .dirtree import generate_ascii_tree
---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dejan",
+    version="0.1.0",
+    description="A collection of utilities for various tasks.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Dan Petrovic",
+    author_email="enquiries@dejanmarketing.com",
+    url="https://github.com/dejanmarketing/dejan", 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
